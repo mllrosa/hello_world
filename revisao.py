8a9cbfe46528a16a5ec3ab383d87decebd08df8e
mllrosa <mllr8sa@gmail.com>
2025-05-07 15:18:17 -0300

    Criacao do arquivo revisao

---
+++ revisao.py
@@ -0,0 +1,18 @@
+#comentário
+
+#variavel é um espaço na memoria que receberá dado
+#tipo : texto, número (int e float(moeda, altura, peso))
+
+#exemplo de número float(flutuante)
+peso = 89.5
+print(peso)
+
+#exemplo de número inteiro
+idade = 18
+print(idade)
+
+#exemplo de variavel do tipo texto
+nome = ("Maria")
+sobrenome = "silva"
+print(nome,sobrenome)
+
