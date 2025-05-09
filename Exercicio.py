d3ce303791cee07487214c6920b7246a43762543
mllrosa <mllr8sa@gmail.com>
2025-05-09 14:01:57 -0300

    Média4

---
+++ Exercicio.py
@@ -0,0 +1,12 @@
+nota1 = int(input('Insira primeira nota:'))
+#print(numero1)
+
+nota2 = float(input('Insira segunda nota:'))
+#print(numero2)
+
+media = nota1 + nota2 / 2
+
+if media >= 7:
+    print("aprovado")
+else:
+    print("reprovado")
